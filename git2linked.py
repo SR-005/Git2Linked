bf1e70f13a0eb409fbef1e2c16fd26f738334400
Sreeram V Gopal <sreeramvg100@gmail.com>
2025-08-10 20:39:58 +0530

    fetching readme data from github

---
+++ git2linked.py
@@ -0,0 +1,32 @@
+import requests
+import base64
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv("GITHUB_TOKEN")
+USERNAME="SR-005"
+
+headers = {"Authorization": f"token {TOKEN}"}
+
+repourl = f"https://api.github.com/users/{USERNAME}/repos"
+reporesponse = requests.get(repourl, headers=headers)
+repos = reporesponse.json()
+
+print("\n-----------Your Repositories-----------")
+for index, repo in enumerate(repos, 1):
+    print(f"{index}. {repo['name']}")
+
+choice = int(input("\nEnter repo number to fetch README: ")) - 1
+reponame = repos[choice]["name"]
+
+readme_url = f"https://api.github.com/repos/{USERNAME}/{reponame}/readme"
+readmeresponse = requests.get(readme_url, headers=headers)
+readmedata = readmeresponse.json()
+
+if "content" in readmedata:
+    content = base64.b64decode(readmedata["content"]).decode("utf-8")
+    print("\n=== README.md ===\n")
+    print(content)
+else:
+    print("No README found for this repository.")
