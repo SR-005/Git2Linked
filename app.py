4e3f5573b56b370dae0a3818bbc651f9f1b10547
Sreeram V Gopal <sreeramvg100@gmail.com>
2025-08-13 00:08:10 +0530

    workflow tuning

--- app.py
+++ app.py
@@ -1,16 +1,24 @@
-from flask import Flask,request
+from flask import Flask,request,redirect,url_for
 import requests
 from dotenv import load_dotenv
 import os
 
+#loading sensitive informations from .env
 load_dotenv()
 CLIENT_ID= os.getenv("CLIENT_ID")
 CLIENT_SECRET= os.getenv("CLIENT_SECRET")
 REDIRECT_URI = "http://localhost:5000/callback"
 
 app=Flask(__name__)
-@app.route("/callback")
 
+#MAIN PAGE:
+@app.route("/",methods=["GET", "POST"])
+def index():
+    auth_url = ("https://www.linkedin.com/oauth/v2/authorization"f"?response_type=code&client_id={CLIENT_ID}"f"&redirect_uri={REDIRECT_URI}&scope=w_member_social")
+    return f'<a href="{auth_url}">Login with LinkedIn</a>'
+
+#OAUTH REDIRECTION
+@app.route("/callback")
 def callback():
     code = request.args.get("code")
     if not code:
@@ -24,26 +32,15 @@ def callback():
         "client_id": CLIENT_ID,
         "client_secret": CLIENT_SECRET
     }
-    token_headers = {
-        "Content-Type": "application/x-www-form-urlencoded"
-    }
+    token_headers = {"Content-Type": "application/x-www-form-urlencoded"}
 
     response = requests.post(token_url, data=token_data, headers=token_headers)
-    if response.status_code != 200:
-        return f"Error getting token: {response.text}"
+    if response.status_code == 200:
+        access_token = response.json().get("access_token")
+        return redirect(url_for('index'))
+    else:
+        return "Failed to get access token"
 
-    access_token = response.json().get("access_token")
-    return f"Access Token: {access_token}"
-
-@app.route("/",methods=["GET", "POST"])
-
-def index():
-    auth_url = (
-        "https://www.linkedin.com/oauth/v2/authorization"
-        f"?response_type=code&client_id={CLIENT_ID}"
-        f"&redirect_uri={REDIRECT_URI}&scope=w_member_social"
-    )
-    return f'<a href="{auth_url}">Login with LinkedIn</a>'
 
 if __name__=="__main__":
     app.run(debug=True)
