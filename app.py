1ec0285a9ac323a13691f8afa1efc72c354fe6a9
Sreeram V Gopal <sreeramvg100@gmail.com>
2025-08-14 21:22:39 +0530

    proxy post testing

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,request,redirect,url_for
+from flask import Flask,request,redirect,url_for,session
 import requests
 from dotenv import load_dotenv
 import os
@@ -10,6 +10,7 @@ CLIENT_SECRET= os.getenv("CLIENT_SECRET")
 REDIRECT_URI = "http://localhost:5000/callback"
 
 app=Flask(__name__)
+app.secret_key = os.urandom(24) 
 
 #MAIN PAGE:
 @app.route("/",methods=["GET", "POST"])
@@ -37,10 +38,34 @@ def callback():
     response = requests.post(token_url, data=token_data, headers=token_headers)
     if response.status_code == 200:
         access_token = response.json().get("access_token")
+        session["access_token"] = access_token
         return redirect(url_for('index'))
     else:
         return "Failed to get access token"
 
+@app.route("/post")
+def post():
+    accesstoken=session.get("access_token")
+    if not accesstoken:
+        redirect(url_for("index"))
+
+    headers = {"Authorization": f"Bearer {accesstoken}","X-Restli-Protocol-Version": "2.0.0"}
+    url = "https://api.linkedin.com/v2/me"
+    response=requests.get(url,headers)
+    if response.status_code!=200:
+        return f"Error getting user URN: {response.text}"
+    
+    userdata = response.json()
+    userurn = userdata.get("id")
+    authorurn = f"urn:li:person:{userurn}"
+    
+    post_text="Hello"
+    payload = {"author": authorurn,"lifecycleState": "PUBLISHED",
+            "specificContent": {"com.linkedin.ugc.ShareContent": {"shareCommentary": {"text": post_text},"shareMediaCategory": "NONE"}},
+        "visibility": {"com.linkedin.ugc.MemberNetworkVisibility": "PUBLIC"}}
+    
+    print(f"✅ Simulated post complete. Payload: {payload}") 
+    return redirect(url_for("index"))
 
 if __name__=="__main__":
     app.run(debug=True)
